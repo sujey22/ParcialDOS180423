029632d1d1f6ae0a42b2624e60b7f96ebf6f0aed
sujey22 <da0089032022@unab.edu.sv>
2023-04-18 21:03:12 -0600

    Creando

---
+++ parcial2/Program.cs
@@ -0,0 +1,11 @@
+﻿int dato1 = 16;
+String dato2 = "25g";
+try
+{
+    int resultado = dato1 + Convert.ToInt32(dato2);
+    Console.WriteLine(resultado);
+}
+catch (Exception)
+{
+    Console.WriteLine("Lo sentimos no se pudo realizar la operación solicitada");
+}
